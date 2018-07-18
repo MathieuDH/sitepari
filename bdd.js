874f21c1ea877b6f125984cac3bc59721a10341a
Mathieu DHondt <dhondtmat@gmail.com>
2018-07-18 15:33:33 +0200

    Ajout bdd avec matchs

---
+++ bdd.js
@@ -0,0 +1,8 @@
+var data = 
+{
+    "matchs":[
+        {"nom":"belgique:angleterre", "coteA":"1.15", "coteB":"1.30", "gagnant":"A"},
+        {"nom":"belgique:bresil", "coteA":"1.18", "coteB":"1.20", "gagnant":"B"},
+        {"nom":"france:croatie", "coteA":"1.02", "coteB":"2.0", "gagnant":"A"}
+    ]
+}
